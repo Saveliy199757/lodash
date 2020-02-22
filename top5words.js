aa0a9a7bc7e3018a091fc57c3445c4bfe6d342af
Saveliy199757 <47079779+Saveliy199757@users.noreply.github.com>
2020-02-22 13:55:18 +0300

    'init'

---
+++ top5words.js
@@ -0,0 +1,17 @@
+const luk = require('./luk')
+const _ = require('lodash')
+
+
+const top5Words = _.flow([
+    _.toLower,
+    _.words,
+    _.countBy,
+    _.toPairs,
+    _.partial(_.orderBy, _, 1, 'desc'),
+    _.partial(_.take, _, 5),
+    _.fromPairs
+
+])
+
+
+console.log(top5Words(luk))
